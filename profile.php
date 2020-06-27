5c42d6390a7df4246d7a5384695198222016ff43
Azamat9904 <azamatnamialy@gmail.com>
2020-06-27 18:27:12 +0600

    Profile file structe

---
+++ profile.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Azamat Namiyaly</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <div class="main-area">
+            <div class="profile-left-wrap">
+                <div class="profile-cover-wrap"></div>
+                <div class="cover-bottom-part"></div>
+                <div class="bio-timeline">
+                    <div class="bio-wrap"></div>
+                    <div class="status-timeline-wrap"></div>
+                </div>
+            </div>
+            <div class="profile-right-wrap"></div>
+        </div>
+        <div class="top-box-show"></div>
+        <div id="adv_dem"></div>
+    </main>
+</body>
+</html>
