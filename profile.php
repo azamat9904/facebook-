7114abd42b47e83aa5c1dd1c905f87447c79b088
Azamat9904 <azamatnamialy@gmail.com>
2020-06-27 23:37:44 +0600

    Profile Header left side was made

--- profile.php
+++ profile.php
@@ -5,10 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/profile.css">
     <title>Azamat Namiyaly</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <div class="top-bar">
+            <div class="top-left-part">
+                <div class="profile-logo"><img src="assets/image/logo.jpg" alt=""></div>
+                <div class="search-wrap">
+                    <input type="text" name="main-search" id = "main-search">
+                    <div class="s-icon top-icon top-css">
+                        <img src="assets/image/icons8-search-36.png" alt="">
+                    </div>
+                </div>
+                <div id="search-show">
+                    <div class="search-result">
+
+                    </div>
+                </div>
+            </div>
+            <div class="top-right-part"></div>
+        </div>
+    </header>
     <main>
         <div class="main-area">
             <div class="profile-left-wrap">
